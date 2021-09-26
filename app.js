de45b31c103f08ddda6223745ad2701f14c82403
Garrett Trott <garretttrott@gmail.com>
2021-09-26 00:34:13 -0700

    test database

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize();
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'movies.db'
+});
+
+// async IIFE
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to database successful')
+  } catch (error) {
+    console.log('Error Connecting to database', error)
+  }
+})();
 
 
