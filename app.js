de662abd2469b0cd7304cbdfaddd7403b6b01360
Garrett Trott <garretttrott@gmail.com>
2021-09-26 15:33:07 -0700

    add model and sync

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const sequelize = new Sequelize({
   storage: 'movies.db'
 });
 
+// Movie Model
+class Movie extends Sequelize.Model {}
+Movie.init({
+  title: Sequelize.STRING
+}, { sequelize });
+
+
 // async IIFE
 (async () => {
+  // Sync all tables
+  await sequelize.sync({ force:true})
+
   try {
-    await sequelize.authenticate();
-    console.log('Connection to database successful')
+    // await sequelize.authenticate();
+    // console.log('Connection to database successful')
+
   } catch (error) {
     console.log('Error Connecting to database', error)
   }
