d88fcccf1de246a64c2c523633f3e3285020a83a
Garrett Trott <garretttrott@gmail.com>
2021-10-05 01:55:08 -0700

    add Comments

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const {Movie, Person} = db.models;
   await db.sequelize.sync({ force:true })
 
   try {
-    const movie = await Movie.create({
+    const movie = await Movie.create({ // Create New person / model / db table
       title: 'Fight Club',
       runtime: 137,
       releaseDate: '1999-11-11',
@@ -21,14 +21,26 @@ const {Movie, Person} = db.models;
     });
     console.log(movie2.toJSON());
 
-    const person = await Person.create({
+    
+    const person = await Person.create({ // Create New person / model / db table
       firstName: "Edward",
       lastName: "Norton"
     });
     console.log(person.toJSON());
+    
+    const movie3 = await Movie.build({ // Build the table before saving 
+      title: 'Kill Bill Vol. 1',
+      runtime: 111,
+      releaseDate: '2003-10-10',
+      isAvailableOnVHS: false,
+    })
+    
+    await movie3.save(); // save the table
+    console.log(movie3.toJSON());
+
 
   } catch (error) {
-    if (error.name === 'SequelizeValidationError') {
+    if (error.name === 'SequelizeValidationError') { // Generate organized Errors
       const errors = error.errors.map(err => err.message);
       console.error('Validation errrors: ', errors);
     } else {
--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'movies.db',
+  dialect: 'sqlite', // what flavor of SQL
+  storage: 'movies.db', // Where to Sore DB
   logging: false
 });
 
@@ -12,6 +12,7 @@ const db = {
   models: {},
 };
 
+// Require Sequelize ORM models 
 db.models.Movie = require('./models/movie.js')(sequelize);
 db.models.Person = require('./models/person.js')(sequelize);
 
