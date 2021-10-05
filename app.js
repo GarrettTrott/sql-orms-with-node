9681c7fdb0452bcdd89cd25c0992b4f37ab12bc5
Garrett Trott <garretttrott@gmail.com>
2021-10-04 20:50:06 -0700

    Add model types and  UUIDs

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ const {Movie} = db.models;
 
   try {
     const movie = await Movie.create({
-        title: 'Fight Club'
-      });
-      console.log(movie.toJSON());
+      title: 'Fight Club',
+      runtime: 139,
+      releaseDate: 1999-11-11,
+      isAvailableOnVHS: true,
+    }); 
+    console.log(movie.toJSON());
 
-      const movie2 = await Movie.create({
-        title: 'The Big Lebowski'
+    const movie2 = await Movie.create({
+      title: 'The Big Lebowski',
+      runtime: 117,
+      releaseDate: 1998-03-06,
+      isAvailableOnVHS: true,
       });
       console.log(movie2.toJSON());
 
--- db/models/movie.js
+++ db/models/movie.js
@@ -3,7 +3,28 @@ const Sequelize = require('sequelize')
 module.exports = (sequelize) => {
   class Movie extends Sequelize.Model {}
   Movie.init({
-    title: Sequelize.STRING,
+    id: {
+      type: DataTypes.UUID,
+      DataTypes.UUIDV1,
+      primaryKey: true,
+    },
+    title: {
+       type: Sequelize.STRING,
+       allowNull: false,
+    },
+    runtime: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    releaseDate: {
+      type: Sequelize.DATEONLY,
+      allowNull: false,
+    },
+    isAvailableOnVHS: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
   }, { sequelize });
 
   return Movie;
